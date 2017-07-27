2ec229976fe9abd272c166e96a69e8f0c6551e08
congfengyin <948629143@qq.com>
2017-07-27 10:06:10 +0800

    修改了获取PIX姿态指针的错误

--- rsi/src/cg_src/offset_calculate.c
+++ rsi/src/cg_src/offset_calculate.c
@@ -28,7 +28,7 @@ float rasY_offsetCalculate(uint8_t openmv_x_offset)
 {
     float angle, y_offset, *height;
     angle = rasAngleCalculate(openmv_x_offset);
-    *ras_get_pix_attitude = getAttitude();
+    ras_get_pix_attitude = getAttitude();
     height = getHeight();
     y_offset = -((*height) * tan( (*ras_get_pix_attitude)[0]))/cos( (*ras_get_pix_attitude)[1] );
     y_offset += ((*height) * sin(angle) * sin(angle + 90 - (*ras_get_pix_attitude)[0]))/(cos( (*ras_get_pix_attitude)[1]) * cos( (*ras_get_pix_attitude)[0]));
@@ -38,7 +38,7 @@ float rasX_offsetCalculate(uint8_t openmv_y_offset)
 {
     float angle, x_offset, *height;
     angle = rasAngleCalculate(openmv_y_offset);
-    *ras_get_pix_attitude = getAttitude();
+    ras_get_pix_attitude = getAttitude();
     height = getHeight();
     x_offset = (*height) * tan( (*ras_get_pix_attitude)[1]);
     x_offset += -((*height) * sin(angle))/(cos( (*ras_get_pix_attitude)[1]) * sin(90 -  (*ras_get_pix_attitude)[1] + angle));
