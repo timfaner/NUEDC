d7b43d4c5b3715a545b72c7aa40a5ab17f40d03a
timfaner <grngrngrngrn@163.com>
2017-07-27 20:58:25 +0800

    更改systemMonitor逻辑

--- rsi/src/cg_src/systemmonitor.c
+++ rsi/src/cg_src/systemmonitor.c
@@ -7,68 +7,102 @@
 #include "systemmonitor.h"
 #include "stdlib.h"
 
+#include "string.h"
+
+#define SPACE 32
+#define ENDL 10
+
 
 //发送指定数据 分别是数据数组 数据个数 类型
 void systemMonitor(uint8_t *arg,int arg_count,uint8_t data_type){
-    static uint8_t send_counter;
-    uint8_t* package;
-
+    static uint8_t send_counter = 'a';
+    uint8_t send_counter_str[4];
+    uint8_t* arg_str = NULL;
+    unsigned char space[2] = {" "};
+    unsigned char endl[2] = {"\n"};
+    uint8_t* package=NULL;
+    int length=0;
+    unsigned char * text=NULL;
+    
+    unsigned char data_type_str[4];
+    
     unsigned long current_time;
+    unsigned char current_time_str[11];
     int i;
-
-    package =  malloc(arg_count + 6);
-
-    for(i = 0;i < arg_count;i++){
-        *(package+i+6) = *(arg + i);
+    
+    package =  (uint8_t*)malloc(arg_count + 25);
+    arg_str = (uint8_t*)malloc(arg_count+1);
+    for(i = 0;i<arg_count;i++){
+         *(arg_str+i) = *(arg+i) ;
     }
-
-    *(package+4) = send_counter++;
-    *(package+5) = data_type;
+    *(arg_str+arg_count) = NULL;
+    
+    
+    
     current_time = millis();
-    for(i = 0;i<4;i++){
-        *(package+i) = (uint8_t) (current_time & 0xff);
-        current_time = current_time >> 8;
-    }
-    SCI5_Serial_Send(package,arg_count+6);
+    sprintf(current_time_str,"%lu",current_time);
+    
+    
+    send_counter_str[0] = send_counter;
+    send_counter_str[1] = NULL;
+    
+    
+    data_type_str[0] = data_type;
+    data_type_str[1] = NULL;
+    
+    strcat(package,current_time_str);
+    strcat(package,space);
+    strcat(package,send_counter_str);
+    strcat(package,data_type_str);
+    strcat(package,arg_str);
+    strcat(package,endl);
+
+	text = package;
+	while(*text!='\0'){
+		length++;
+		text++;
+	}
+	SCI5_Serial_Send(package,length);
+    
     free(package);
     package = NULL;
+    free(arg_str);
+    arg_str = NULL;
+    send_counter++;
 }
+
 
 void systemDataUpdate(float *data,int data_count,uint8_t data_type){
     unsigned char package[12][20];
-    unsigned char send[256];
+    unsigned char send[50]={0} ;
     int i = 0;
-    int j = 0;
-    int k = 0;
-    int *length;
-	unsigned char * text;
-	
-	length = malloc(data_count);
-    for(i = 0;i< data_count;i++){
-        uart_5_printf(package[i],"%.2f",*(data+i));
-        text = package[i];
-        while(*text!='\0'){
-		    length[i]++;
-		    text++;
-	    }
-        package[i][length[i]] = 255;
+
+    int length;
+    unsigned char * text = send;
+    char comma[2] = ",";
     
+    for(i = 0;i< data_count-1;i++){
+        sprintf(package[i],"%.2f",*(data+i));
+        strcat(package[i],comma);
+        strcat(send,package[i]);
     }
-    for(i = 0;i< data_count;i++){
-        for(j = 0;j<length[i]+1;j++){
-            send[k] = package[i][j];
-            k++;
-        }
+    sprintf(package[data_count-1],"%.2f",*(data+data_count-1));
+    strcat(send,package[data_count-1]);
+
+    
+    while(*text!='\0'){
+        length++;
+        text++;
     }
-    systemMonitor(send,k,data_type);
+    systemMonitor(send,length,data_type);
 }
 
 void systemEventUpdate(uint16_t new_event){
     uint8_t package[2];
-
+    
     system_event |= new_event;
     package[0] =(uint8_t) (system_event & 0xff);
-    package[0] =(uint8_t) ((system_event>>8) & 0xff);
+    package[1] =(uint8_t) ((system_event>>8) & 0xff);
     systemMonitor(package,2,MONITOR_EVENT);
 }
 
@@ -77,4 +111,3 @@ void systemErrorUpdate(uint8_t error){
     errorcode = error;
     systemMonitor(&errorcode,1,MONITOR_ERROR);
 }
-
